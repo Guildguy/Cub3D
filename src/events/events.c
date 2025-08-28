2cd1d26ba9d296082e439a21a6d5a891a2ef4e63
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 17:59:10 -0300

    update(key_handler): starts the function movement_handler()

--- src/events/events.c
+++ src/events/events.c
@@ -6,5 +6,7 @@ int	key_handler(int keysym, t_cub *cub)
 		close_handler(cub);
 	else if (keysym == XK_Right || keysym == XK_Left)
 		rotation_handler(keysym, cub);
+	else if (keysym == XK_s || keysym == XK_S || keysym == XK_w || keysym == XK_W)
+		movement_handler(keysym, cub);
 	return (0);
 }
