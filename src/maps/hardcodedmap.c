86fdf82a9b8184221085cd5801cb86db0f72a75d
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 20:07:01 -0300

    update(new wall): added a wall at the center of the map

--- src/maps/hardcodedmap.c
+++ src/maps/hardcodedmap.c
@@ -9,7 +9,7 @@ char	**create_hard_map(void)
 	map[1] = ft_strdup("1S0000001");
 	map[2] = ft_strdup("101000101");
 	map[3] = ft_strdup("111000111");
-	map[4] = ft_strdup("100000001");
+	map[4] = ft_strdup("100010001");
 	map[5] = ft_strdup("101000101");
 	map[6] = ft_strdup("111111111");
 	map[7] = NULL;
