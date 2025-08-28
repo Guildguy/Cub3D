aa40ed45c010d29cc53833619a0b5817c93dd014
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 20:06:20 -0300

    feat(macro.h): added a new header that stores macros

---
+++ includes/macros.h
@@ -0,0 +1,20 @@
+#ifndef MACROS_H
+# define MACROS_H
+
+# define WIDTH 1000
+# define HEIGHT 1000
+
+# define HIT 1
+# define NO_HIT 0
+# define NO_SO_WALL 1
+# define ES_WE_WALL 0
+# define WALL 49
+# define NORTH_WALL 1
+# define SOUTH_WALL -1
+# define WEST_WALL -1
+# define EAST_WALL 1
+
+# define R_SPEED 0.1
+# define M_SPEED 0.1
+
+#endif
