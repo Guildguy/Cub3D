a5b7508f210553830274f7bb871835061f3e50b6
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 19:45:39 -0300

    fix(norm)

--- src/events/movement_event.c
+++ src/events/movement_event.c
@@ -7,16 +7,16 @@ static void	forward_backward_move(t_cub *cub, double direction)
 
 	new_y = cub->player.pos_y + cub->player.dir_y * M_SPEED * direction;
 	new_x = cub->player.pos_x + cub->player.dir_x * M_SPEED * direction;
-	if (cub->map[(int)cub->player.pos_y][(int)new_x] != '1')
+	if (cub->map[(int)cub->player.pos_y][(int)new_x] != WALL)
 		cub->player.pos_x = new_x;
-    if (cub->map[(int)new_y][(int)cub->player.pos_x] != '1')
-        cub->player.pos_y = new_y;
+	if (cub->map[(int)new_y][(int)cub->player.pos_x] != WALL)
+		cub->player.pos_y = new_y;
 }
 
 void	movement_handler(int keysym, t_cub *cub)
 {
 	if (keysym == XK_w || keysym == XK_W)
-		forward_backward_move(cub, 	1.0);
+		forward_backward_move(cub, 1.0);
 	else if (keysym == XK_s || keysym == XK_S)
 		forward_backward_move(cub, -1.0);
 }
--- src/events/rotate_event.c
+++ src/events/rotate_event.c
@@ -8,11 +8,15 @@ static void	rotate_player(t_cub *cub, double direction)
 
 	angle = R_SPEED * direction;
 	old_dir_x = cub->player.dir_x;
-	cub->player.dir_x = cub->player.dir_x * cos(angle) - cub->player.dir_y * sin(angle);
-	cub->player.dir_y = old_dir_x * sin(angle) + cub->player.dir_y * cos(angle);
+	cub->player.dir_x = cub->player.dir_x * cos(angle) \
+	- cub->player.dir_y * sin(angle);
+	cub->player.dir_y = old_dir_x * sin(angle) \
+	+ cub->player.dir_y * cos(angle);
 	old_plane_x = cub->player.cam_plane_x;
-	cub->player.cam_plane_x = cub->player.cam_plane_x * cos(angle) - cub->player.cam_plane_y * sin(angle);
-	cub->player.cam_plane_y = old_plane_x * sin(angle) + cub->player.cam_plane_y * cos(angle);
+	cub->player.cam_plane_x = cub->player.cam_plane_x \
+	* cos(angle) - cub->player.cam_plane_y * sin(angle);
+	cub->player.cam_plane_y = old_plane_x * sin(angle) \
+	+ cub->player.cam_plane_y * cos(angle);
 }
 
 void	rotation_handler(int keysym, t_cub *cub)
