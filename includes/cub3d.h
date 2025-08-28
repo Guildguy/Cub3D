006204822b6896f182c8d3065cecec09d6710c32
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 20:05:41 -0300

    update(modularization): macros removed n' passed to another header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -25,17 +25,7 @@
 # include "../minilibx-linux/mlx.h"
 
 # include "libft.h"
-
-# define WIDTH 1000
-# define HEIGHT 1000
-
-# define HIT 1
-# define NO_HIT 0
-# define NO_SO_WALL 1
-# define ES_WE_WALL 0
-
-# define R_SPEED 0.1
-# define M_SPEED 0.15
+# include "macros.h"
 
 /* Structs */
 typedef struct s_img
