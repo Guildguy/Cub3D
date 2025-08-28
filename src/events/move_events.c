892024fafb14dc218f86b9ce1a05821348fc2bc3
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 00:03:26 -0300

    feat(rotate player): added an aux function that calculates the retotation of the player using trigonometry

--- src/events/move_events.c
+++ src/events/move_events.c
@@ -1,2 +1,16 @@
 #include "cub3d.h"
 
+static void	rotate_player(t_cub *cub, double direction)
+{
+	double	angle;
+	double	old_dirX;
+	double	old_planeX;
+
+	angle = R_SPEED * direction;
+	old_dirX = cub->player.dir_x;
+	cub->player.dir_x = cub->player.dir_x * cos(angle) - cub->player.dir_y * sin(angle);
+	cub->player.dir_y = old_dirX * sin(angle) + cub->player.dir_y * cos(angle);
+	old_planeX = cub->player.cam_plane_x;
+	cub->player.cam_plane_x = cub->player.cam_plane_x * cos(angle) - cub->player.cam_plane_y * sin(angle);
+	cub->player.cam_plane_y = old_planeX * sin(angle) + cub->player.cam_plane_y * cos(angle);
+}
