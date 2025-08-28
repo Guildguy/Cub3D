170805c27a7b6695795ecb3a0d8b7f3cc951ef78
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 00:11:29 -0300

    update(key_handler): added rotation_handler to it

--- src/events/events.c
+++ src/events/events.c
@@ -4,6 +4,8 @@ int	key_handler(int keysym, t_cub *cub)
 {
 	if (keysym == ESC)
 		close_handler(cub);
+	else if (keysym == RIGHT || keysym == LEFT)
+		rotation_handler(keysym, cub);
 	return (0);
 }
 
