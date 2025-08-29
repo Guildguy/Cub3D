b97fa71d678246e3519c28f2285e676fc1837060
Fabio Luiz Leite Junior <fabiolljunior23@gmail.com>
2025-08-28 22:20:30 -0300

    feat(Libft): added new functions to aux the parsing

--- includes/libft.h
+++ includes/libft.h
@@ -3,6 +3,7 @@
 
 /* Headers */
 # include "cub3d.h"
+# include "get_next_line.h"
 
 // Libft/libft.c
 bool	ft_isspace(int c);
@@ -10,5 +11,14 @@ void	ft_putstr_fd(char *s, int fd);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlen(const char *str);
 void	ft_bzero(void *ptr, size_t n);
+int		ft_atoi(const char *str);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	**ft_split(char *s, char delim);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
